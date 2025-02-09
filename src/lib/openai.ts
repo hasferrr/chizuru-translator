32ab1210d197bcca13338065970c233c49edfb6c
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-02-09 19:18:20 +0700

    refactor: change OpenAI instance to a Map of baseURLs

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,13 +1,16 @@
 import OpenAI from 'openai'
 
-let instance: OpenAI | null = null
+let instance: Map<string, OpenAI> = new Map()
 
-export const openai = (baseURL: string) => {
-  if (!instance) {
-    instance = new OpenAI({
-      apiKey: process.env.OPENAI_API_KEY,
+export const openai = (baseURL: string): OpenAI => {
+  if (!instance.has(baseURL)) {
+    instance.set(
       baseURL,
-    })
+      new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+        baseURL,
+      }),
+    )
   }
-  return instance
+  return instance.get(baseURL)!
 }
