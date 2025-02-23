02394bca4e5a46c22124a4855fa48db92b2a1d39
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-02-23 09:54:19 +0700

    feat(app): increase the size limit of the request body

--- src/app/app.ts
+++ src/app/app.ts
@@ -32,7 +32,7 @@ app.use(cors({
   allowedHeaders: ['Authorization', 'Content-Type']
 }))
 
-app.use(express.json())
+app.use(express.json({ limit: "1mb" }))
 
 // Routes
 app.use('/api', translateRoutes)
