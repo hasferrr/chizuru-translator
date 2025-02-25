fe6c9271a554afe5e3738bdf6200cbc9932c619c
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-02-25 21:07:22 +0700

    feat: update model configuration in env to use JSON format

--- src/app/model-map.ts
+++ src/app/model-map.ts
@@ -1,20 +1,28 @@
-const modelForApi = process.env.FREE_MODEL_NAME_SHOWN_FOR_API?.split(',') || []
-const modelList = process.env.FREE_MODEL_NAME?.split(',') || []
-const baseUrlList = process.env.FREE_MODEL_BASE_URL?.split(',') || []
-const apiKeyList = process.env.FREE_API_KEY?.split(',') || []
+import { z } from "zod"
 
-interface ModelData {
-  model: string
-  baseURL: string
-  apiKey: string
+const modelDataSchema = z.object({
+  name: z.string(),
+  model: z.string(),
+  baseURL: z.string(),
+  apiKey: z.string(),
+})
+
+type ModelData = z.infer<typeof modelDataSchema>
+
+let modelArray: ModelData[] = []
+try {
+  if (process.env.FREE_MODEL_JSON === undefined) {
+    console.log("FREE_MODEL_JSON is not defined")
+  }
+  const json = JSON.parse(process.env.FREE_MODEL_JSON ?? "[]")
+  modelArray = z.array(modelDataSchema).parse(json)
+} catch (e) {
+  console.error(e)
 }
 
 export const modelMap = new Map<string, ModelData>()
 
-for (let i = 0; i < modelForApi.length; i++) {
-  modelMap.set(modelForApi[i], {
-    model: modelList[i],
-    baseURL: baseUrlList[i],
-    apiKey: apiKeyList[i],
-  })
+for (const model of modelArray) {
+  modelMap.set(model.name, model)
 }
+console.log(modelMap.keys())
